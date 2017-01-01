9cdae6acedb4c30e7b12aaaff7852d664d73179c
Martin Urbanec <martin.urbanec@wikimedia.cz>
2017-01-01 01:14:07 +0100

    Should be saved to /Odkazy

--- update_disambigs.sh
+++ update_disambigs.sh
@@ -4,5 +4,5 @@
 ./script3.py
 python get.py Wikipedista:UrbanecmBot/DisambigLinks | sed -e s/odkazu/odkazů/g > out
 ./script4.py
-python ~/pywikibot/scripts/add_text.py -always -text:"$(cat out)" -page:"Wikipedista:UrbanecmBot/DisambigLinks"
+python ~/pywikibot/scripts/add_text.py -always -text:"$(cat out)" -page:"Wikipedie:Seznam nejvíce odkazovaných rozcestníků/Odkazy"
 rm out
