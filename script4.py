41c0c6117eddab0abca8af2d1b6b19858bc492a5
Martin Urbanec <martin.urbanec@wikimedia.cz>
2017-02-07 11:55:56 +0100

    Complete rewrite, delete all files

--- script4.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-#-*- coding: utf-8 -*-
-
-import pywikibot
-
-site = pywikibot.Site('cs', 'wikipedia')
-page = pywikibot.Page(site, u"Wikipedista:UrbanecmBot/DisambigLinks")
-page.text = u""
-page.save(u"vyprázdnění stránky")
--- update_disambigs.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-./script2.py
-./script3.py
-python get.py Wikipedista:UrbanecmBot/DisambigLinks | sed -e s/odkazu/odkazů/g > out
-./script4.py
-python ~/pywikibot/scripts/add_text.py -always -text:"$(cat out)" -page:"Wikipedie:Seznam nejvíce odkazovaných rozcestníků/Odkazy"
-rm out
